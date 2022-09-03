3b8f17c971d136de1538d81260bf36e945d49d23
PNUHyeonWoo <102347501+PNUHyeonWoo@users.noreply.github.com>
2022-09-03 19:01:57 +0900

    Create nt_1019_bookPage.cpp

---
+++ cpp/gold/gold1/nt_1019_bookPage.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	long long* result = new long long[10];
+	for (int i = 0; i < 10; i++)
+		result[i] = 0;
+	long long inp;
+	cin >> inp;
+	long long now = inp;
+	long long degree = 1;
+	while (now > 0)
+	{
+		long long tmp = now / 10;
+		for (int i = 0; i < 10; i++)
+			result[i] += tmp * degree;
+		result[0] -= degree;
+		tmp = inp % (degree * 10);
+		int max = tmp / degree;
+		for (int i = 0; i < max; i++)
+			result[i] += degree;
+		result[max] += (tmp % degree)+1;
+
+		degree *= 10;
+		now /= 10;
+	}
+
+	for (int i = 0; i < 10; i++)
+		cout << result[i] << " ";
+
+}
